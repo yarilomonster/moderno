fff58a914eba1a7f80e8b052546873cc08bd0e8f
yarilomonster <doc09dayz@gmail.com>
2019-11-25 14:52:56 +0300

    add section product

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,8 @@ gulp.task('script', function() {
 gulp.task('js', function() {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/mixitup/dist/mixitup.js'
+        // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
