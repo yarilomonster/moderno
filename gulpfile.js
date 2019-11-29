34243999e9df6de8beb7057a06847882736a3a1b
yarilomonster <doc09dayz@gmail.com>
2019-11-30 00:58:33 +0300

    added new-items page

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('css', function() {
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
         // 'node_modules/maginif-popup/dist/magnific-popup.css'
     ])
         .pipe(concat('_libs.scss'))
@@ -50,6 +51,7 @@ gulp.task('js', function() {
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
     ])
         .pipe(concat('libs.min.js'))
